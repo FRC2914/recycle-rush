959abac226ba96dafc4fa51e0ee83fdb778430d4
tshapinsky <tobiasshapinsky@gmail.com>
2015-01-15 18:48:41 -0500

    added command support and pneumatic support along with lots of juicy documentation

---
+++ src/net/frc2914/commands/CommandManager.java
@@ -0,0 +1,59 @@
+package net.frc2914.commands;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class CommandManager {
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface Command {
+		public String value();
+	}
+
+	private static Map<String, InvokableMethod> commands = new HashMap<String, InvokableMethod>();
+
+	private static void loadCommandsFromClass(Class clazz, Object invoker) {
+		Stream.of(clazz.getMethods())
+				.filter(m -> m.getAnnotationsByType(Command.class).length > 0)
+				.forEach(
+						m -> {
+							Stream.of(m.getAnnotationsByType(Command.class))
+									.map(a -> (Command)a)
+									.forEach(
+											a -> commands.put(a.value(), new InvokableMethod(m, invoker)));
+						});
+		
+	}
+	/**
+	 * load commands from non static class (only use for singletons)
+	 * @param container instance of class with methods
+	 */
+	public static void loadCommandsFromClass(Object container){
+		loadCommandsFromClass(container.getClass(), container);
+	}
+	
+	/**
+	 * load commands from static class
+	 * @param clazz class with methods
+	 */
+	public static void loadCommandsFromClass(Class clazz){
+		loadCommandsFromClass(clazz, null);
+	}
+	
+	public static void call(String command){
+		String commandName = command.substring(0, command.indexOf(' '));
+		command = command.replaceFirst(commandName, "");
+		command = command.trim();
+		try {
+			System.out.println("command: " + commandName);
+			commands.get(commandName).invoke(command.split(" "));
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/net/frc2914/robot/subsystems/Subsystem.java
@@ -0,0 +1,5 @@
+package net.frc2914.robot.subsystems;
+
+public abstract class Subsystem {
+	public abstract void init();
+}
