8a3059442d869dcdc965266a79e6d19e35e6a83c
squilter <s.quilter@aol.com>
2015-01-30 20:27:18 -0500

    yellow thresholding

--- src/net/frc2914/services/VisionService.java
+++ src/net/frc2914/services/VisionService.java
@@ -1,27 +1,45 @@
 package net.frc2914.services;
 
 import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.ColorMode;
 import com.ni.vision.NIVision.Image;
-import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.image.BinaryImage;
 
 public class VisionService extends Service {
+	private static final int HUE_UPPER = 35;//35 to 50
+	private static final int HUE_LOWER = 50;
+	
 	int session;
 	Image frame;
+	Image binaryFrame;
 	
 	@Override
 	public void update() {
-		NIVision.Rect rect = new NIVision.Rect(10,10,100,100);
 		NIVision.IMAQdxGrab(session, frame, 1);
-		NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-		CameraServer.getInstance().setImage(frame);
+		NIVision.Range hue = new NIVision.Range(HUE_UPPER,HUE_LOWER);
+		NIVision.Range saturation = new NIVision.Range(150,255);
+		NIVision.Range value = new NIVision.Range(150,255);
+		
+		NIVision.imaqColorThreshold(binaryFrame, frame, 255,ColorMode.HSV, hue, saturation, value);
+		/*
+		 * TODO:
+		 * contour detection.  select biggest contour.  ignore if too small.
+		 * find x-coord of both sides of yellow contour.  draw those lines.
+		 * calculate approximately how far we are and the angle 
+		 * 
+		 * set exposure to manual
+		 * Calibrate
+		 * Optimize
+		 */
+		CameraServer.getInstance().setImage(binaryFrame);
 	}
 
 	@Override
 	public void init() {
 		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+		binaryFrame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_U8, 100);
 		session = NIVision.IMAQdxOpenCamera("cam0",
 				NIVision.IMAQdxCameraControlMode.CameraControlModeController);
 		NIVision.IMAQdxConfigureGrab(session);
